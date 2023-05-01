e5920aad178a958408d7a58d47319ddaf8db9ccd
lemonsauce21 <lemonsauce21@gmail.com>
2023-05-01 23:54:27 +0900

    simpleTest

---
+++ src/main/java/Solution1.java
@@ -0,0 +1,11 @@
+public class Solution1 {
+    public static void main(String[] args){
+
+        solution_1(7,2);
+    }
+
+    public static void solution_1(int num1, int num2) {
+        int answer = num1 % num2;
+        System.out.println("answer = " + answer);
+    }
+}
